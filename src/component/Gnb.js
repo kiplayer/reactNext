cf8fdd99dfc11bd81e886eaca22aca2a19e20ee3
kiplayer <kiplayer@naver.com>
2021-04-12 15:21:43 +0900

    next

--- src/component/Gnb.js
+++ src/component/Gnb.js
@@ -2,17 +2,17 @@ export default function Gnb() {
 	return (
 		<div id="gnb">
 			<ul>
-				<li><a href="/class_jsx/">JSX</a></li>
-				<li><a href="/class_component/">Component</a></li>
-				<li><a href="/class_css/">CSS</a></li>
-				<li><a href="/class_event/">Event</a></li>
-				<li><a href="/class_state/">State</a></li>
-				<li><a href="/class_props/">Props</a></li>
-				<li><a href="/class_map/">Map</a></li>
-				<li><a href="/class_route/">Route</a></li>
-				<li><a href="/class_effect/">useEffect</a></li>
-				<li><a href="/class_fetch/">fetch()</a></li>
-				<li><a href="/class_hooks/">Hooks</a></li>
+				<li><a href="./class_jsx/">JSX</a></li>
+				<li><a href="./class_component/">Component</a></li>
+				<li><a href="./class_css/">CSS</a></li>
+				<li><a href="./class_event/">Event</a></li>
+				<li><a href="./class_state/">State</a></li>
+				<li><a href="./class_props/">Props</a></li>
+				<li><a href="./class_map/">Map</a></li>
+				<li><a href="./class_route/">Route</a></li>
+				<li><a href="./class_effect/">useEffect</a></li>
+				<li><a href="./class_fetch/">fetch()</a></li>
+				<li><a href="./class_hooks/">Hooks</a></li>
 			</ul>
 		</div>
 	);
